90881644646130083dfed72084cb74e86c5e2f7f
christiandrey <oluwaseun.adedire@gmail.com>
2021-01-23 02:18:35 +0100

    feat(header): Add Header and Button components

---
+++ src/core/utils/index.ts
@@ -0,0 +1,43 @@
+import formatRelative from 'date-fns/formatRelative'
+
+// ----------------------------------------------------------------
+// UTILITY
+// ----------------------------------------------------------------
+
+export function nil<T>(value: T): boolean {
+  return typeof value === 'undefined' || value === null
+}
+
+export function notNil<T>(value: T): boolean {
+  return !nil(value)
+}
+
+// ----------------------------------------------------------------
+// OBJECT
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+// DATETIME
+// ----------------------------------------------------------------
+
+export function fromNow(dateTime: ValidDate): string {
+  return formatRelative(new Date(dateTime), new Date())
+}
+
+// ----------------------------------------------------------------
+// STRING
+// ----------------------------------------------------------------
+
+export function toTitleCase(text: string) {
+  if (nil(text)) {
+    return text
+  }
+  return `${text[0]?.toUpperCase()}${text?.substr(1).toLowerCase()}`
+}
+
+export function toCapitalizedFirst(text: string) {
+  if (nil(text)) {
+    return text
+  }
+  return `${text[0]?.toUpperCase()}${text?.substr(1)}`
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
-import * as React from 'react'
+import React, { useRef, useState } from 'react'
 import classnames from 'classnames'
 import 'tailwindcss/tailwind.css'
+import Header from './modules/header'
 
 interface Props {
   className?: string
 }
 
 export const DocmakerEditor = ({ className }: Props) => {
+  const createdAt = useRef(new Date().toISOString()).current
+
+  const [title, setTitle] = useState('')
+
   return (
-    <div className={classnames('font-sans bg-blue-500', className)}>
-      Example Component goes here
+    <div className={classnames('font-sans', className)}>
+      <Header title={title} createdAt={createdAt} onChangeTitle={setTitle} />
     </div>
   )
 }
